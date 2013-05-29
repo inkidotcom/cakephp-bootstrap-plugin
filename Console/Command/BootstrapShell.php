b453ca86cea27abe4eb28478c2afabe793f2b0f6
Manny Ramirez <inki@inki.com>
2013-05-29 15:42:48 -0700

    Initial plugin files

---
+++ Console/Command/BootstrapShell.php
@@ -0,0 +1,11 @@
+<?php
+class BootstrapShell extends AppShell {
+	public function main() {
+		$this->out('Hello world.');
+	}
+
+	public function hey_there() {
+		
+		$this->out('Hey there ' . $this->args[0]);
+	}
+}
