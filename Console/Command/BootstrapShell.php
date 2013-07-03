b57235bf356cee6f83ba7f6dd763b0a919a5bb8a
Manny Ramirez <inki@inki.com>
2013-07-02 17:36:11 -0700

    added command shells (cake bootstrap copy, cake bootstrap build)

--- Console/Command/BootstrapShell.php
+++ Console/Command/BootstrapShell.php
@@ -1,11 +1,38 @@
 <?php
 class BootstrapShell extends AppShell {
 	public function main() {
-		$this->out('Hello world.');
+		$this->out('options [copy|build]');
 	}
 
-	public function hey_there() {
-		
-		$this->out('Hey there ' . $this->args[0]);
+	public function copy() {
+		$this->out("<info>Starting copy...</info>\n");
+		$vendor_path = $this->getBootstrapPath();
+		$css_dir = $vendor_path . 'bootunstrap' . DS . 'css/';
+		$js_dir = $vendor_path . 'bootunstrap' . DS . 'js/';
+		$this->out("copying css: \n" . $css_dir . "\nto\n" . CSS);
+		$this->out(shell_exec("cp -R " . $css_dir . " " . CSS));
+		$this->out("copying js: \n" . $js_dir . "\nto\n" . JS);
+		$this->out(shell_exec("cp -R " . $js_dir . " " . JS));
+		$this->out("<info>Copy done.</info>\n");
+	}
+
+	public function build() {
+		$this->out("<info>Starting build...</info>\n");
+		$vendor_path = $this->getBootstrapPath();
+		$bun = $vendor_path . 'bootunstrap';
+		$this->out(shell_exec("cd " . $bun . " && ./build.sh bootstrap\n"));
+		$this->out("<info>Build done.</info>\n");
+	}
+
+	private function getBootstrapPath() {
+		$vendor_dirs = App::path('Vendor');
+		if (count($vendor_dirs) > 1) {
+			foreach ($vendor_dirs as $key => $value) {
+				if (strpos($value, 'Bootstrap/Vendor') > 1) {
+					return $value;
+				}
+			}
+		}
+		return null;
 	}
 }
